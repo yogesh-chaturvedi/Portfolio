43cf6f5b897197967b5e37e415b343501f2a5b48
yogesh-chaturvedi <yogeshchaturvedi2425@gmail.com>
2025-07-31 17:52:48 +0530

    Commit some UI changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -19,20 +19,20 @@ const Projects = () => {
 
             <div className="projects flex flex-col gap-5 flex-wrap justify-center md:flex-row md:gap-20">
                 {/* first */}
-                <div className='ProjectCard flex flex-col gap-6 bg-gray-200 dark:bg-black dark:text-white text-slate-900 rounded-2xl border-2 border-black dark:border-white h-[400px] w-[300px]'>
-                    <img className='border-b-2 border-gray-600 dark:border-white rounded-t-2xl h-[50%] w-full' src={screenshots.geminiSS} alt="gemini-img" />
+                <div className='ProjectCard flex flex-col gap-6 bg-gray-200 dark:bg-black dark:text-white text-slate-900 rounded-2xl border-2 border-black  dark:border-white h-[400px] w-[300px]'>
+                    <img className='border-b-2 border-gray-600 dark:border-white rounded-t-2xl h-[50%] w-full' src={screenshots.shopNest} alt="gemini-img" />
 
                     <div className='flex justify-evenly px-2'>
-                        <span className='border-2 bg-gray-800 text-slate-100 text-xs rounded-xl px-3 py-1.5 '>React</span>
-                        <span className='border-2 bg-gray-800 text-slate-100 text-xs rounded-xl px-3 py-1.5 '>Tailwind</span>
-                        <span className='border-2 bg-gray-800 text-slate-100 text-xs rounded-xl px-3 py-1.5 '>Gemini Api</span>
+                        <span className='border-2 bg-gray-800 text-slate-100 text-xs rounded-xl px-3 py-1.5 '>MERN</span>
+                        <span className='border-2 bg-gray-800 text-slate-100 text-xs rounded-xl px-3 py-1.5 '>Stripe</span>
+                        <span className='border-2 bg-gray-800 text-slate-100 text-xs rounded-xl px-3 py-1.5 '>Gemini API</span>
                     </div>
 
                     <div className='px-2'>
-                        <div className='text-2xl font-bold'>AI Chatbot</div>
-                        <div className='text-sm'>Created an AI chatbot using Gemini API, featuring real-time chat, clean design, and state management with Context API.</div>
+                        <div className='text-xl font-bold mb-1'>AI-Powered E-Commerce</div>
+                        <div className='text-sm'>ShopNest is a responsive e-commerce site with auth, filters, pagination, Stripe, admin panel, and Gemini AI chatbot.</div>
                     </div>
-                    <div onClick={() => { handleClick('https://gemini-clone-omega-ruddy.vercel.app/') }}><i className="fa-brands fa-github text-3xl cursor-pointer pb-3"></i></div>
+                    <div onClick={() => { handleClick('https://shop-nest-livid.vercel.app/') }}><i className="fa-brands fa-github text-3xl cursor-pointer pb-3"></i></div>
                 </div>
 
                 {/* second */}
@@ -46,30 +46,30 @@ const Projects = () => {
                     </div>
 
                     <div className='px-2'>
-                        <div className='text-2xl font-bold'>Password Manager</div>
+                        <div className='text-xl font-bold mb-1'>Password Manager</div>
                         <div className='text-sm'>Developed LockVault – a MERN stack password manager with Encryption, Authentication, CRUD, and responsive UI.</div>
                     </div>
                     <div onClick={() => { handleClick('https://password-manager-ten-xi.vercel.app/') }}><i className="fa-brands fa-github text-3xl cursor-pointer pb-3"></i></div>
                 </div>
 
                 {/* third */}
-                <div className='ProjectCard flex flex-col gap-6 bg-gray-200 dark:bg-black dark:text-white text-slate-900 rounded-2xl border-2 border-black  dark:border-white h-[400px] w-[300px]'>
-                    <img className='border-b-2 border-gray-600 dark:border-white rounded-t-2xl h-[50%] w-full' src={screenshots.shopNest} alt="gemini-img" />
+                <div className='ProjectCard flex flex-col gap-6 bg-gray-200 dark:bg-black dark:text-white text-slate-900 rounded-2xl border-2 border-black dark:border-white h-[400px] w-[300px]'>
+                    <img className='border-b-2 border-gray-600 dark:border-white rounded-t-2xl h-[50%] w-full' src={screenshots.geminiSS} alt="gemini-img" />
 
                     <div className='flex justify-evenly px-2'>
-                        <span className='border-2 bg-gray-800 text-slate-100 text-xs rounded-xl px-3 py-1.5 '>MERN</span>
-                        <span className='border-2 bg-gray-800 text-slate-100 text-xs rounded-xl px-3 py-1.5 '>Stripe</span>
-                        <span className='border-2 bg-gray-800 text-slate-100 text-xs rounded-xl px-3 py-1.5 '>Context Api</span>
+                        <span className='border-2 bg-gray-800 text-slate-100 text-xs rounded-xl px-3 py-1.5 '>React</span>
+                        <span className='border-2 bg-gray-800 text-slate-100 text-xs rounded-xl px-3 py-1.5 '>Tailwind</span>
+                        <span className='border-2 bg-gray-800 text-slate-100 text-xs rounded-xl px-3 py-1.5 '>Gemini Api</span>
                     </div>
 
                     <div className='px-2'>
-                        <div className='text-2xl font-bold'>E-Commerce</div>
-                        <div className='text-sm'>ShopNest is a responsive e-commerce site with authentication, search, sorting, filtering, Stripe payments, and an admin panel.</div>
+                        <div className='text-xl font-bold mb-1'>AI Chatbot</div>
+                        <div className='text-sm'>Created an AI chatbot using Gemini API, featuring real-time chat, clean design, and state management with Context API.</div>
                     </div>
-                    <div onClick={() => { handleClick('https://shop-nest-livid.vercel.app/') }}><i className="fa-brands fa-github text-3xl cursor-pointer pb-3"></i></div>
+                    <div onClick={() => { handleClick('https://gemini-clone-omega-ruddy.vercel.app/') }}><i className="fa-brands fa-github text-3xl cursor-pointer pb-3"></i></div>
                 </div>
-            </div>
 
+            </div>
 
             <button onClick={() => { handleClick('https://github.com/yogesh-chaturvedi') }} className='contactMe mr-5 dark:bg-gradient-to-b from-blue-400 to-indigo-600  dark:hover:from-blue-500 dark:hover:to-indigo-700 bg-gray-800 hover:bg-gray-950 text-slate-100 dark:text-white  transition-all duration-300 shadow-md px-4 py-2 rounded-lg cursor-pointer'>Check My GitHub <i className="fa-solid fa-arrow-right ml-2"></i></button>
 
