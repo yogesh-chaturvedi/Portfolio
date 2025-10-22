f276d94ed50a506b46a19a2e410be2e47c9d7b4f
yogesh-chaturvedi <yogeshchaturvedi2425@gmail.com>
2025-10-22 22:14:51 +0530

    Update project

--- src/assets/screenshots.js
+++ src/assets/screenshots.js
@@ -1,11 +1,13 @@
 import geminiSS from './geminiSS.png'
 import passwordManagerSS from './passwordManagerSS.png'
 import shopNest from './shopNest.png'
+import learnifyAiSS from './learnifyAiSS.png'
 
 
 
 export const screenshots = {
     geminiSS,
     passwordManagerSS,
-    shopNest
+    shopNest,
+    learnifyAiSS
 }
